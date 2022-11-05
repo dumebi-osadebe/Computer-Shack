e1653f0a13d68fc108f6a061b5c8742dfd05888a
Dumebi Winifred <winifred2412@gmail.com>
2022-11-05 15:35:58 -0400

    atomic propositions for a product
    Atomic propositions for a product in the predicate form: product(ProductName, Manufacturer, Type, Price, Rating), defines a product with given ProductName (e.g Macbook Pro), Manufacturer (e.g. Apple), Type (e.g. laptop), Price (e.g. $2000), and average customer Rating out of 5 (e.g. 4.2).  Underscores are used in places of spaces for the values of constants.

---
+++ Computer_Shack_.pl
@@ -0,0 +1,13 @@
+
+%%%%% atomic propositions for a product
+product(macbook_pro, apple, laptop, 2400, 4.0).
+product(macbook_air, apple, laptop, 1700, 4.8).
+product(microsoft_surface_go, microsoft, laptop, 1500, 3.6).
+product(lenovo_thinkpad, lenovo, laptop, 1400, 4.8).
+product(tech_tablet, tech_net, laptop, 1000, 2.5).
+product(galaxy_tab_10, samsung, tablet, 510, 3.9).
+product(ipad_air, apple, tablet, 749, 4.7).
+product(ipad_mini, apple, tablet, 549, 4.1).
+product(three_ft_hdmi_cable, sony, cable, 20, 4.0).
+product(two_ft_hdmi_cable, sony, cable, 15, 3.9).
+product(one_ft_hdmi_cable, sony, cable, 10, 3.5).
